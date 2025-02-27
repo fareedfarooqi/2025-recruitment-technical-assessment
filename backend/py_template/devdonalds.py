02335169b1faf21ab4df413c80da0cb85ae86557
Fareed Farooqi <z5425725@ad.unsw.edu.au>
2025-02-27 15:11:27 +1100

    Passing all tests for task 2. But need to utilise data classes.

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -6,7 +6,9 @@ import re
 # ==== Type Definitions, feel free to add or modify ===========================
 @dataclass
 class CookbookEntry:
+	type: str
 	name: str
+	requiredItems: List
 
 @dataclass
 class RequiredItem():
@@ -19,8 +21,9 @@ class Recipe(CookbookEntry):
 
 @dataclass
 class Ingredient(CookbookEntry):
+	type: str
+	name: str
 	cook_time: int
-
 
 # =============================================================================
 # ==== HTTP Endpoint Stubs ====================================================
@@ -28,7 +31,7 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+cookbook = []
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -64,9 +67,47 @@ def parse_handwriting(recipeName: str) -> Union[str | None]:
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	"""
+	This function is used to create a 
+	"""
+	# I need to get the JSON body that is sent over HTTP to us.	
+	data = request.get_json()
+	
+	# So I need to validate that the JSON we receive has the 'type' key and the appropriate values.
+	if 'type' not in data:
+		return jsonify({"error": "The type is missing! Please add the type to your request!!!"}), 400
+	elif data['type'] not in ['recipe', 'ingredient']:
+		return jsonify({"error": "Please use the specified type of either 'recipe' or 'ingredient'."}), 400
+	
+	if data['type'] == "ingredient" and data['cookTime'] < 0:
+		return jsonify({"error": "Please enter a valid 'cookTime' that is greater or equal to 0!!!"}), 400
+	
+	# We also need to check if the entry we are making in the cookbook is unique.
+	for entry in cookbook:
+		if entry.get("name") == data["name"]:
+			return jsonify({"error": "An entry of this name already exists. Please provide unique entry names!!!"}), 400
 
+	# Before adding the item to our cookbook, we need to see if there are any duplicates in 'requiredItems'.
+	requiredItems = data.get("requiredItems", [])
+
+	if validate_required_items(requiredItems):
+		return jsonify({"error": "There are duplicate entires in 'requiredItems'. It can only contain unique elements!!!"}), 400
+
+	# All cases above pass so we can add the entry into our cookbook.
+	cookbook.append(data)
+	
+	return 'success', 200
+
+def validate_required_items(requiredItems):
+	seen = set()
+
+	for item in requiredItems:
+		item_name = item.get("name")
+		if item_name in seen:
+			return True
+		seen.add(item_name)
+		
+	return False
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
